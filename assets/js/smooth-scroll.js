7424cd18f7111158f34874c93c3e262f0a47e7a9
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-01-24 22:02:25 -0300

    Updated Script

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -1,11 +1,18 @@
-jQuery(document).ready((function ($) {
-  $("#smoothScroll").click((function (event) {
-    event.preventDefault(), $("html, body").animate({
-      scrollTop: 0
-    }, 200)
-  })), $(".scroll").click((function (event) {
-    event.preventDefault(), $("html, body").animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000)
-  }))
-}));
+/* Smoth scroll */
+jQuery(document).ready(function($) {
+    $("#smoothScroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    });
+
+    $(".scroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 3000);
+    });
+});
+
+const iconLink = document.querySelector('.iconLink');
+
+iconLink.addEventListener('click', (e) => {
+    e.preventDefault();
+})
