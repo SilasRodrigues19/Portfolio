8fdbe8335cfb424c1110ee034150cdaca081b869
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-02-06 18:28:19 -0300

    :rewind: Revert Changes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,24 +1,23 @@
-const themeToggle = document.querySelector('#theme-toggle')
+const themeToggle = document.getElementById('theme-toggle')
 
 const darkTheme = 'dark-theme',
-    lightThemeIcon = 'uil-sun',
-    darkThemeIcon = 'uil-moon';
+    ligthTheme = 'uil-sun';
 
+// localStorage variables
 const selectedTheme = localStorage.getItem('selected-theme'),
     selectedIcon = localStorage.getItem('selected-icon');
 
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light',
-    getCurrentIcon = () => themeToggle.classList.contains(lightThemeIcon) ? darkThemeIcon : lightThemeIcon;
+    getCurrentIcon = () => themeToggle.classList.contains(ligthTheme) ? 'uil-moon' : 'uil_sun';
 
 if (selectedTheme) {
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeToggle.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](lightThemeIcon);
+    themeToggle.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](ligthTheme);
 }
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle(darkTheme);
-    themeToggle.classList.toggle(darkThemeIcon);
-    themeToggle.classList.toggle(lightThemeIcon);
+    themeToggle.classList.toggle(ligthTheme);
 
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
