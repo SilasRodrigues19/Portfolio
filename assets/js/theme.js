77bb02568f62775201326e4d91f21018cdf5a31b
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-08-04 18:32:11 -0300

    refactor: correct the spelling

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,31 +1,36 @@
-const themeToggle = document.getElementById('theme-toggle')
+const themeToggle = document.getElementById('theme-toggle');
 
 const darkTheme = 'dark-theme',
-    ligthTheme = 'bi-cloud-sun';
+  lightTheme = 'bi-cloud-sun';
 
 // localStorage variables
 const selectedTheme = localStorage.getItem('selected-theme'),
-    selectedIcon = localStorage.getItem('selected-icon');
+  selectedIcon = localStorage.getItem('selected-icon');
 
 const getCurrentTheme = () =>
     document.body.classList.contains(darkTheme) ? 'dark' : 'light',
   getCurrentIcon = () =>
-    themeToggle.classList.contains(ligthTheme)
+    themeToggle.classList.contains(lightTheme)
       ? 'bi-cloud-moon'
       : 'bi-cloud-sun';
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeToggle.classList[selectedIcon === 'bi-cloud-moon' ? 'add' : 'remove'](ligthTheme);
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeToggle.classList[selectedIcon === 'bi-cloud-moon' ? 'add' : 'remove'](
+    lightTheme
+  );
 }
 
 themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme);
-    themeToggle.classList.toggle(ligthTheme);
+  document.body.classList.toggle(darkTheme);
+  themeToggle.classList.toggle(lightTheme);
 
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
 });
 
-
-!navigator.userAgent.match(/Mobile/) ? themeToggle.innerText = '' : themeToggle.innerHTML = '&nbsp;&nbsp;Tema'
+!navigator.userAgent.match(/Mobile/)
+  ? (themeToggle.innerText = '')
+  : (themeToggle.innerHTML = '&nbsp;&nbsp;Tema');
